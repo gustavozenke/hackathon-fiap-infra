e223901c3c0f56600601ffc9de662061185318ae
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-01-29 16:32:40 -0300

    infra cognito

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -1,50 +1,48 @@
 resource "aws_cognito_user_pool" "user_pool" {
-  name = "user_pool_usuarios_processamento_video"
+  name = "user-pool"
 
-  schema {
-    name                = "email"
-    attribute_data_type = "String"
-    mutable             = true
-    required            = true
-  }
-
-  schema {
-    name                = "phone_number"
-    attribute_data_type = "String"
-    mutable             = true
-    required            = true
-  }
-
-  password_policy {
-    minimum_length    = 8
-    require_lowercase = true
-    require_numbers   = true
-    require_symbols   = true
-    require_uppercase = true
-  }
-
+  username_attributes = ["email"]
   auto_verified_attributes = ["email"]
+  password_policy {
+    minimum_length = 6
+  }
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_subject = "Account Confirmation"
+    email_message = "Your confirmation code is {####}"
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
 }
 
-resource "aws_cognito_user_pool_client" "user_pool_client" {
-  name                                 = "user_pool_usuarios_processamento_video_client"
-  user_pool_id                         = aws_cognito_user_pool.user_pool.id
-  generate_secret                      = false
-  allowed_oauth_flows                  = ["code"]
-  allowed_oauth_scopes                 = ["email", "openid", "profile"]
-  allowed_oauth_flows_user_pool_client = true
-  callback_urls                        = ["https://oauth.pstmn.io/v1/callback"]
-  logout_urls                          = ["https://example.com/logout"]
-  supported_identity_providers         = ["COGNITO"]
-}
+resource "aws_cognito_user_pool_client" "client" {
+  name = "cognito-client"
 
-resource "aws_cognito_user_pool_domain" "user_pool_domain" {
-  domain       = "my-user-pool-domain"
   user_pool_id = aws_cognito_user_pool.user_pool.id
+  generate_secret = false
+  refresh_token_validity = 90
+  prevent_user_existence_errors = "ENABLED"
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH"
+  ]
+
 }
 
-resource "aws_cognito_user_group" "user_group" {
-  name         = "standard_users"
-  user_pool_id = aws_cognito_user_pool.user_pool.id
-  description  = "Grupo padrão para usuários cadastrados."
+resource "aws_cognito_user_pool_domain" "cognito-domain" {
+  domain       = "gabrielaraujo"
+  user_pool_id = "${aws_cognito_user_pool.user_pool.id}"
 }
