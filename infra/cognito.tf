e8bb6688e0c02359009dbb0a4c57ef008c3b5e63
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-01-29 20:32:59 -0300

    infra cognito

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -1,10 +1,16 @@
 resource "aws_cognito_user_pool" "user_pool" {
   name = "user-pool"
 
-  username_attributes = ["email"]
+  username_attributes = ["mail"]
   auto_verified_attributes = ["email"]
+
   password_policy {
-    minimum_length = 6
+    minimum_length                   = 8
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = true
+    require_uppercase                = true
+    temporary_password_validity_days = 7
   }
 
   verification_message_template {
@@ -25,13 +31,32 @@ resource "aws_cognito_user_pool" "user_pool" {
       max_length = 256
     }
   }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "phone_number"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 20
+    }
+  }
+
+  username_configuration {
+    case_sensitive = false
+  }
+
+  mfa_configuration = "OFF"
 }
 
 resource "aws_cognito_user_pool_client" "user_pool_client" {
   name         = "appClient"
   user_pool_id = aws_cognito_user_pool.user_pool.id
 
-  generate_secret = false
+  generate_secret = true
 
   allowed_oauth_flows = ["code", "implicit"]
   allowed_oauth_flows_user_pool_client = true
