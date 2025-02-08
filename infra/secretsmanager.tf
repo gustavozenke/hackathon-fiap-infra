4868921dfa9cf2ff53b51858f3cd55bbf8d47885
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-08 15:46:17 -0300

    Secrets manager sms

--- infra/secretsmanager.tf
+++ infra/secretsmanager.tf
@@ -1,12 +1,14 @@
-resource "aws_secretsmanager_secret" "email_secret" {
-  name        = "email-credentials"
-  description = "Secret for email credentials"
+resource "aws_secretsmanager_secret" "sms_secret" {
+  name        = "ss-credentials"
+  description = "Secrets ara armazenar credenciais para envio de comunicacao SMS"
 }
 
-resource "aws_secretsmanager_secret_version" "email_secret_version" {
-  secret_id     = aws_secretsmanager_secret.email_secret.id
+resource "aws_secretsmanager_secret_version" "sms_secret_version" {
+  secret_id     = aws_secretsmanager_secret.sms_secret.id
   secret_string = jsonencode({
-    EMAIL_USER    = "email-default@gmail.com",
-    EMAIL_PASSWORD = "senha-default"
+    USER_POOL_ID    = "default",
+    TWILIO_ACCOUNT_SID = "default",
+    TWILIO_AUTH_TOKEN = "default",
+    TWILIO_PHONE_NUMBER = "default"
   })
 }
