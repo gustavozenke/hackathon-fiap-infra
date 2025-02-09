0ba6ea95f1a24eca180e8e45e409e02825b76119
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-09 08:20:09 -0300

    Delete SNS nao utilizado

--- infra/sns.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_sns_topic" "comunicacao_cliente_email_topic" {
-  name = "hackathon-comunicacao-cliente"
-}
-
-output "sns_topic_arn" {
-  value = aws_sns_topic.comunicacao_cliente_email_topic.arn
-}
-
